f15e693f4b8d65b96cbf7d6b41d2dde15b213754
Ilyagu <inagdimaev@mail.ru>
2022-05-15 23:13:10 +0300

    lol

--- front/src/component/App.js
+++ front/src/component/App.js
@@ -38,7 +38,7 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <div className="app">
+            <div>
                 <div className="app__tapbar">
                     <TapBar reserveFlag={true} mainPage={this.state.mainPage} tapBarClick={this.tapBarClick} />
                 </div>
